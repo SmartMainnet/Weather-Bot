e8abf22f9d9eb4ef80b626f47b01c2236eb2fe1d
SmartMainnet <SmartMainnet@gmail.com>
2022-07-31 01:53:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 require('dotenv').config()
-const { START_MESSAGE, ERROR_MESSAGE, STICKER, BOT_API, WEATHER_API, MONGODB_PASSWORD } = process.env
-
-const TelegramApi = require('node-telegram-bot-api')
-const axios = require('axios')
+const { STICKER, BOT_API, WEATHER_API, MONGODB_URI } = process.env
 
 const { MongoClient } = require('mongodb')
-const url = `mongodb+srv://admin:${MONGODB_PASSWORD}@database.owkg3.mongodb.net/test`
+const client = new MongoClient(MONGODB_URI)
 
-const client = new MongoClient(url)
 client.connect()
-
 const db = client.db('weather-bot')
 
 const users = db.collection('users')
 const info = db.collection('info')
 
+const axios = require('axios')
+const TelegramApi = require('node-telegram-bot-api')
+
 const token = BOT_API
 const bot = new TelegramApi(token, { polling: true })
 
 function translit(word){
-	var answer = '';
-	var converter = {
+	let answer = ''
+	let converter = {
 		'а': 'a',    'б': 'b',    'в': 'v',    'г': 'g',    'д': 'd',
 		'е': 'e',    'ё': 'e',    'ж': 'zh',   'з': 'z',    'и': 'i',
 		'й': 'y',    'к': 'k',    'л': 'l',    'м': 'm',    'н': 'n',
@@ -36,82 +34,105 @@ function translit(word){
 		'У': 'U',    'Ф': 'F',    'Х': 'H',    'Ц': 'C',    'Ч': 'Ch',
 		'Ш': 'Sh',   'Щ': 'Sch',  'Ь': '',     'Ы': 'Y',    'Ъ': '',
 		'Э': 'E',    'Ю': 'Yu',   'Я': 'Ya'
-	};
+	}
  
-	for (var i = 0; i < word.length; ++i ) {
+	for (let i = 0; i < word.length; ++i ) {
 		if (converter[word[i]] == undefined){
-			answer += word[i];
+			answer += word[i]
 		} else {
-			answer += converter[word[i]];
+			answer += converter[word[i]]
 		}
 	}
  
-	return answer;
+	return answer
 }
 
-bot.on('message', async (msg) => {
+let isPost = false
 
+bot.on('message', async (msg) => {
     const text = msg.text
     const chatId = msg.chat.id
 
     if (text === '/start') {
-
         await bot.sendSticker(chatId, STICKER)
         await bot.sendMessage(chatId,
-            `Привет ${msg.from.first_name}${(msg.from.last_name === undefined) ? '': ` ${msg.from.last_name}`}!`
-            + '\n' +
-            'Это погодный бот.'
-            + '\n' +
-            'Автор: @SmartMainnet'
+            `👋 Привет ${msg.from.first_name}${(msg.from.last_name === undefined) ? '': ` ${msg.from.last_name}`}!\n` +
+            '🌤 Это погодный бот.\n' +
+            '👨‍💻 Автор: @SmartMainnet'
         )
-        await bot.sendMessage(chatId, START_MESSAGE)
+        await bot.sendMessage(chatId,
+            'Отправь мне название города,\n' +
+            'например Москва,\n' +
+            'чтобы узнать его погоду.'
+        )
 
-        const document = await users.findOne({ id: chatId })
-
-        if (document === null) {
-            await users.insertOne({
-                id: chatId,
-                username: msg.from.username,
-                first_name: msg.from.first_name,
-                last_name: msg.from.last_name,
-            })
-            await info.updateOne({}, { $inc: { users: 1 } })
-        }
-    
+        await users.findOne({ id: chatId }).then(async res => {
+            if (res === null) {
+                await users.insertOne({
+                    id: chatId,
+                    username: msg.from.username,
+                    first_name: msg.from.first_name,
+                    last_name: msg.from.last_name,
+                    start_date: new Date(),
+                    calls: 0
+                })
+                await info.updateOne({}, { $inc: { users: 1 } })
+            }
+        })
+    } else if (text === '/post' && msg.from.username === 'SmartMainnet') {
+        await bot.sendMessage(chatId, 'Отправь мне пост')
+        isPost = true
+    } else if (isPost === true && msg.from.username === 'SmartMainnet') {
+        users.find().toArray(async (err, res) => {
+            for (let user of res) {
+                let chatId = user.id
+                await bot.sendMessage(chatId, text)
+            }
+        })
+        isPost = false
     } else {
+        await users.findOne({ id: chatId }).then(async res => {
+            if (res === null) {
+                await users.insertOne({
+                    id: chatId,
+                    username: msg.from.username,
+                    first_name: msg.from.first_name,
+                    last_name: msg.from.last_name,
+                    start_date: new Date(),
+                    calls: 0
+                })
+                await info.updateOne({}, { $inc: { users: 1 } })
+            }
+        })
 
         try {
-
             let city = translit(text)
             let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${WEATHER_API}`
 
             await axios.get(url).then(async res => {
                 await bot.sendMessage(chatId,
-                    `Погода: ${Math.round(res.data.main.temp)}°C  -  ${res.data.weather[0].description}`
-                    + '\n' +
-                    `Ветер: ${res.data.wind.speed} км/ч`
-                    + '\n' +
-                    `Влажность: ${res.data.main.humidity}%`
+                    `☁️ Погода: ${res.data.weather[0].description}\n` +
+                    `☀️ Температура: ${Math.round(res.data.main.temp)}°C\n` +
+                    `💨 Ветер: ${(res.data.wind.speed / 3.6).toFixed(1)} м/с\n` +
+                    `💧 Влажность: ${res.data.main.humidity}%\n`
                 )
             })
 
             await users.updateOne({ id: chatId },
                 {
                     $set: {
-                        id: chatId,
                         username: msg.from.username,
                         first_name: msg.from.first_name,
                         last_name: msg.from.last_name,
+                        date_last_call: new Date(),
+                        last_call: text
                     },
-                    $inc: { calls: 1 },
+                    $inc: { calls: 1 }
                 }
             )
             await info.updateOne({}, { $inc: { calls: 1 } })
-
         } catch {
-            await bot.sendMessage(chatId, ERROR_MESSAGE)
+            await bot.sendMessage(chatId, 'Это не город!')
         }
-
     }
-
 })
